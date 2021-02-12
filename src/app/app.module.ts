49a6c5c41ec50bcff989d84ebf6006e3bb1613c0
Gabriel Sousa <gabrieldesousa05@gmail.com>
2021-02-12 03:55:00 -0300

    login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { NavbarComponent } from './login/navbar/navbar.component';
+import { FooterComponent } from './login/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
